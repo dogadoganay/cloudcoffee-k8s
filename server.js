462b284d74b10bf182cbd3956cdea789138cd409
Doğa Doğanay <pj30369@gmail.com>
2026-05-15 12:14:26 +0300

    Initial CloudCoffee project

--- server.js
+++ server.js
@@ -1,15 +1,56 @@
-'use strict';
+var express = require('express')
+var fs = require('fs')
+var path = require('path')
 
-const express = require('express');
+var app = express()
+var PORT = 8080
 
-// Constants
-const PORT = 8080;
+var DATA_DIR = process.env.DATA_DIR || path.join(__dirname, 'data')
+var REVIEWS_FILE = path.join(DATA_DIR, 'reviews.json')
 
-// App
-const app = express();
-app.get('/', function (req, res) {
-  res.send('Hello world\n');
-});
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.listen(PORT);
-console.log('Running on http://localhost:' + PORT);
+if (!fs.existsSync(DATA_DIR)) {
+  fs.mkdirSync(DATA_DIR, { recursive: true })
+}
+
+if (!fs.existsSync(REVIEWS_FILE)) {
+  fs.writeFileSync(REVIEWS_FILE, '[]')
+}
+
+app.get('/api/reviews', function (req, res) {
+  var reviews = JSON.parse(fs.readFileSync(REVIEWS_FILE))
+  res.json(reviews)
+})
+
+app.post('/api/reviews', function (req, res) {
+  var reviews = JSON.parse(fs.readFileSync(REVIEWS_FILE))
+
+  var newReview = {
+    id: Date.now(),
+    name: req.body.name || 'Anonymous',
+    message: req.body.message || '',
+    createdAt: new Date().toISOString()
+  }
+
+  reviews.push(newReview)
+  fs.writeFileSync(REVIEWS_FILE, JSON.stringify(reviews, null, 2))
+
+  res.json(newReview)
+})
+
+app.delete('/api/reviews/:id', function (req, res) {
+  var reviews = JSON.parse(fs.readFileSync(REVIEWS_FILE))
+  reviews = reviews.filter(function (review) {
+    return String(review.id) !== String(req.params.id)
+  })
+
+  fs.writeFileSync(REVIEWS_FILE, JSON.stringify(reviews, null, 2))
+  res.json({ success: true })
+})
+
+app.listen(PORT, function () {
+  console.log('CloudCoffee running on http://localhost:' + PORT)
+})
